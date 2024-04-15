027a6af2cdff760424e15d117e05642c971bf5c9
Hillal-cell <sserunjogi.hillal@students.mak.ac.ug>
2024-04-16 01:44:05 +0300

    Fetched the values to use from the tables for use in the diffrent types of Graphs on the dashboard.

--- ResultsManager/app/Http/Controllers/HomeController.php
+++ ResultsManager/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Mark;
+use App\Models\Student;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,25 @@ class HomeController extends Controller
     public function index()
     {
 
-        $marks = Mark::all();
-        return view('dashboard',compact('marks'));
+        $totalStudents = Student::count();
+
+        // Retrieve all students from the database
+        $students = Student::all();
+        
+
+       
+    // Extracting subject names from the database table
+    $subjectNames = ['english', 'science', 'maths', 'sst'];
+
+   
+    
+    // Extracting marks for each subject
+$subjectMarks = $students->map(function ($student) use ($subjectNames) {
+    return collect($subjectNames)->map(function ($subject) use ($student) {
+        return $student->$subject;
+    });
+});
+
+        return view('dashboard', compact('students', 'totalStudents', 'subjectNames', 'subjectMarks'));
     }
 }
